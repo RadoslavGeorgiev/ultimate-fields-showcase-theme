d522eb6e05d30b82043c58030f7229dede6abea5
Radoslav Georgiev <rageorgiev@gmail.com>
2018-02-17 23:18:19 +0100

    Added styles to the team page

--- modules/team/template.php
+++ modules/team/template.php
@@ -1,15 +1,17 @@
-<div class="section main">
+<div class="section main team">
     <div class="section__center">
         <?php while( have_groups( 'team' ) ): the_group() ?>
 
             <?php if( 'department' == get_group_type() ): ?>
 
-                <h2><?php the_sub_value( 'name' ) ?></h2>
+                <h2 class="team__department"><?php the_sub_value( 'name' ) ?></h2>
 
             <?php else: ?>
 
-                <h3><?php the_sub_value( 'first_name' ) ?> <?php the_sub_value( 'last_name' ) ?></h3>
-                <?php the_sub_value( 'bio' ) ?>
+				<div class="team__member">
+	                <h3><?php the_sub_value( 'first_name' ) ?> <?php the_sub_value( 'last_name' ) ?></h3>
+	                <?php the_sub_value( 'bio' ) ?>
+				</div>
 
             <?php endif ?>
 
